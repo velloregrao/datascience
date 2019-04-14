1c45e81c91cdf5cd88136cc1a24e014cc7d32d07
velloregrao <velloregrao@gmail.com>
2019-04-14 10:38:44 -0400

    Enumerate, Zip Features

--- Data Scientist with python/Python Data Science Toolbox Part 2/Using Iterators in Python Land/Iterators.py
+++ Data Scientist with python/Python Data Science Toolbox Part 2/Using Iterators in Python Land/Iterators.py
@@ -2,3 +2,26 @@
 word = 'data'
 it = iter(word)
 print(*it)
+
+avengers = ['hawkeye', 'Captain America', 'Ironman', 'Black Widow', 'Black Panther']
+
+releases = ['avengers', 'avengers - age of ultraon', 'avengers - infinity war', 'avengers - end game']
+
+marvel = iter(avengers)
+
+marvelmovies = zip(avengers, releases)
+
+print(next(marvel))
+print(next(marvel))
+print(next(marvel))
+print(next(marvel))
+print(next(marvel))
+
+marvel = list(enumerate(avengers))
+
+print(marvel)
+
+for item in marvel:
+    print(item)
+
+print(*marvelmovies)
