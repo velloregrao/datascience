22239d8f5dfafb361ab1b4605f9811e0bb8a415a
velloregrao <velloregrao@gmail.com>
2019-09-25 21:41:11 -0400

    Dash App Updates

--- Dash Apps Using Flask/Basic Dash App/application.py
+++ Dash Apps Using Flask/Basic Dash App/application.py
@@ -1,47 +1,100 @@
-from flask import Flask
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+import boto3
+import s3fs
 
+s3 = boto3.client('s3')
 
-app = dash.Dash(__name__)
+print(dcc.__version__) # 0.6.0 or above is required
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 application = app.server
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-    html.Div(children='Dash: A web application framework for Python.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-    dcc.Graph(
-        id='Graph1',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
+app.layout = html.Div([
+    # represents the URL bar, doesn't render anything
+    dcc.Location(id='url', refresh=False),
+    # content will be rendered in this element
+    html.Div(id='page-content')
 ])
 
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
+#df = pd.read_csv('gdp-life-exp-2007.csv')
+df = pd.read_csv('s3://gpgdatascience/gdp-life-exp-2007.csv')
+nfl_df = pd.read_csv('s3://gpgdatascience/NFL_Data_Science_2018_2019.csv', header=1)
+nfl_df.drop(nfl_df.columns[0], axis=1, inplace=True)
+nfl_df.drop([0], axis=0, inplace=True)
+nfl_df.drop("Opposing Team Win %", axis=1, inplace=True)
+
+x =  nfl_df['PPG']
+y = nfl_df['PA']
+
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+def display_page(pathname):
+    print(pathname)
+    if pathname == '/life-expectancy':
+        print(pathname)
+        return html.Div([
+                dcc.Graph(
+                    id='life-exp-vs-gdp',
+                    figure={
+                        'data': [
+                            go.Scatter(
+                                x=df[df['continent'] == i]['gdp per capita'],
+                                y=df[df['continent'] == i]['life expectancy'],
+                                text=df[df['continent'] == i]['country'],
+                                mode='markers',
+                                opacity=0.7,
+                                marker={
+                                    'size': 15,
+                                    'line': {'width': 0.5, 'color': 'white'}
+                                },
+                                name=i
+                            ) for i in df.continent.unique()
+                        ],
+                        'layout': go.Layout(
+                            xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                            yaxis={'title': 'Life Expectancy'},
+                            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                            legend={'x': 0, 'y': 1},
+                            hovermode='closest'
+                        )
+                    }
+                )
+            ])
+    elif pathname == '/nfl':
+        print(pathname)
+        return html.Div([
+                    dcc.Graph(
+                        id='NFL_Data_Science_2018_2019',
+                        figure={
+                            'data': [
+                                go.Bar(
+                                    x=x,
+                                    y=y,
+                                    text=y
+                                )
+                            ],
+                            'layout': go.Layout(
+                                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                                yaxis={'title': 'Life Expectancy'},
+                                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                                legend={'x': 0, 'y': 1},
+                                hovermode='closest'
+                            )
+                        }
+                    )
+            ])
+    else:
+        print("Data Science")
+        return html.Div([
+            html.H3('You are on page {}'.format(pathname))
+        ])
+
+
+if __name__ == '__main__':
     application.run(debug=True)
